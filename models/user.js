9dfbecf0591b1b7f7dead28f197ec429a3c05aac
ankitsaini-github <ankit121saini@gmail.com>
2024-08-23 15:24:57 +0530

    addToCart functionality added

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id= new mongodb.ObjectId(id);
   }
 
   save() {
@@ -13,6 +15,28 @@ class User {
       .collection("users")
       .insertOne(this);
   }
+
+  addToCart(product){
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    })
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if(cartProductIndex >=0){
+      newQuantity = this.cart.items[cartProductIndex].quantity+1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    }else{
+      updatedCartItems.push({productId: new mongodb.ObjectId(product._id), quantity: newQuantity})
+    }
+
+    const updatedCart = {items: updatedCartItems}
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne({_id: this._id},{$set: {cart: updatedCart}});
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
