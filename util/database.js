8b4009785c14ce0d70e6d24d519afffc884360db
ankitsaini-github <ankit121saini@gmail.com>
2024-08-23 09:51:48 +0530

    mongodb connect

---
+++ util/database.js
@@ -0,0 +1,17 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.mongoClient;
+
+const mongoConnect = (callback)=>{
+  MongoClient.connect(
+    "mongodb+srv://user1:mongouser1@cluster0.goaussj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client)=>{
+      console.log('connected to mongodb')
+      callback(client)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+module.exports = mongoConnect;
