be8c28b139b6e3c6b0610f0a546a23060358a192
ankitsaini-github <ankit121saini@gmail.com>
2024-08-23 11:26:24 +0530

    post and get products

--- util/database.js
+++ util/database.js
@@ -1,17 +1,31 @@
 const mongodb = require("mongodb");
+
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback)=>{
+
   MongoClient.connect(
-    "mongodb+srv://user1:mongouser1@cluster0.goaussj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://user1:mongouser1@cluster0.goaussj.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
   )
     .then((client)=>{
       console.log('connected to mongodb')
-      callback(client)
+      _db = client.db();
+      callback(_db);
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No db found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
