e028e487c92a075cb8d6b0bafd60e456e15b5eff
ankitsaini-github <ankit121saini@gmail.com>
2024-08-23 11:53:36 +0530

    fetch single product by id

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require("../util/database").getDb;
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -25,9 +26,24 @@ class Product {
       .collection("products")
       .find()
       .toArray()
-      .then(products=>{
-        console.log(products)
+      .then((products) => {
+        console.log(products);
         return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: mongodb.ObjectId.createFromHexString(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
       })
       .catch((err) => {
         console.log(err);
