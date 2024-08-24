b9be441fab521e23599eafb3307dc4d05672c885
ankitsaini-github <ankit121saini@gmail.com>
2024-08-24 14:14:00 +0530

    order system updated to mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,9 +1,10 @@
 const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
-      console.log(products)
+      console.log(products);
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
@@ -45,7 +46,7 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .populate('cart.items.productId')
+    .populate("cart.items.productId")
     .then((user) => {
       // console.log(user.cart.items)
       res.render("shop/cart", {
@@ -65,7 +66,7 @@ exports.postCart = (req, res, next) => {
     })
     .then((result) => {
       console.log(result);
-      res.redirect('/cart')
+      res.redirect("/cart");
     });
 };
 
@@ -81,18 +82,32 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .addOrder()
-    .then((result) => {
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: {...i.productId._doc} };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then(()=>{
       res.redirect("/orders");
     })
     .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
-    .then((orders) => {
-      res.render("shop/orders", {
+  Order.find({'user.userId': req.user._id}).then((orders)=>{
+        res.render("shop/orders", {
         path: "/orders",
         pageTitle: "Your Orders",
         orders: orders,
